46a7c005d5dc531e37a37d253e2358bf47e6d31d
ayden <ad0988877@otc.edu>
2024-12-02 09:44:19 -0600

    second commit

--- dash.php
+++ dash.php
@@ -11,7 +11,27 @@ try {
     echo "Error connecting to the database: " . $e->getMessage();
 }
 
+$query = "SELECT * FROM contacts";
+$contacts =$db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($contacts)) {
+    echo "No employees found in the database.";
+    exit;
+}
 ?>
+
+<ul class="list">
+    <?php foreach ($contacts as $contact): ?>
+        <li class="data">
+            <h3><?php echo $contact['contact_id']; ?></h3>
+            <p>first name: <?php echo $contact['contact_first_name']; ?></p>
+            <p>last name: <?php echo $contact['contact_last_name']; ?></p>
+            <p>phone number: <?php echo $contact['contact_phone']; ?></p>
+            <p>address: <?php echo $contact['contact_adrress']; ?></p>
+            <p>service id: <?php echo $contact['service_id']; ?></p>
+        </li>
+    <?php endforeach; ?>
+</ul>
 
 <!DOCTYPE html>
 <html lang="en">
